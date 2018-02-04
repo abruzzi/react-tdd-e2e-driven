50f105325e4c973a3cd06dca62e2730157f7820e
Juntao Qiu <juntao.qiu@gmail.com>
2018-02-04 12:34:26 +0800

    make sure the route is actually working

--- src/App.e2e.spec.js
+++ src/App.e2e.spec.js
@@ -57,7 +57,13 @@ describe('Bookish', () => {
 
       const url = await page.evaluate('location.href')
       expect(url).toEqual(`${appUrlBase}/books/1`)
-  }, 10000)
+
+      await page.waitForSelector('.description')
+      const result = await page.evaluate(() => {
+          return document.querySelector('.description').innerText
+      })
+      expect(result).toEqual('')
+  })
 })
 
 afterAll(() => {
